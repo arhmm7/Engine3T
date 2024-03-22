0964061b51101ef02ef5695901ed3d06fdcd59f8
arhmm7 <arhmm7@gmail.com>
2024-03-23 01:14:56 +0530

    Updated TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,28 +1,23 @@
 import java.util.*;
 class TicTacToe {
-	TicTacToe() {
-	}
 	public static void main(String[] args) {
 		Board br = new Board();
 		GameLogic gl = new GameLogic();
 		Scanner scn = new Scanner(System.in);
-		br.Init();
-		String Player_1,Player_2 = "Null";
+
 		System.out.println("Select Player : \n1) X \n2) O");
 		int ply = scn.nextInt();
-		if (ply == 1) {
-			Player_2 = "O";
-			Player_1 = "X";
-		} 
-		else {
-			Player_1 = "O";
-			Player_2 = "X";
-		}
+		String Player_1 = (ply==1) ? "X" : "O";
+		String Player_2 = (ply==1) ? "O" : "X";
+
 		System.out.println("Game Starts :");
+		br.Init();
 		br.printBoard();
+
 		String[][] tempBoard = new String[3][3];
 		int inp = 0, move = 0;
 		boolean gameRunning = true , PLAYER_1_PLAYS = true;
+
 		while (gameRunning) {
 			tempBoard = br.getBoard();
 			if (!gl.checkWin(tempBoard) && move < 9) {
@@ -42,7 +37,7 @@ class TicTacToe {
 				}
 			} 
 			else {
-				if (move == 9) {
+				if (move >= 9) {
 					System.out.println("Draw");
 					gameRunning = false;
 
